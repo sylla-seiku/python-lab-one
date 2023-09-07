54726ee07d19e1e556323dd44347486c4588417d
sylla-seiku <seikusylla@gmail.com>
2023-09-07 14:58:21 -0400

    Creating diffrent variables

--- python.py
+++ python.py
@@ -1,8 +1,12 @@
-price = 10 #in programing this number is known as an interger
-rating = 4.9 #in programing this number is known as a float. and the 'rating part is known as a variable.
-name = 'Seiku' #in programing this here 'Seiku' is known as a strig
-is_published = True #in programing these are boolean values 'True/False' 
-print(price)
+full_name = 'Seiku Sillah'
+age = 30
+is_new = True
+
+#price = 10 #in programing this number is known as an interger
+#rating = 4.9 #in programing this number is known as a float. and the 'rating part is known as a variable.
+#name = 'Seiku' #in programing this here 'Seiku' is known as a strig
+#is_published = True #in programing these are boolean values 'True/False' 
+#print(price)
 
 # these below is me learning about the "the print function".
 #print("Seiku Sillah") 
