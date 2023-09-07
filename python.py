c8b241710eac02ea43d6c22fa01928354344bdfb
sylla-seiku <seikusylla@gmail.com>
2023-09-07 14:49:27 -0400

    Learning Variables and differnt values.

--- python.py
+++ python.py
@@ -1,9 +1,17 @@
-print("Seiku Sillah") 
-print('o----')
-print(' ||||')
-print('*' * 10)
-print(' ** ** ')
-print('*  *  *')
-print(' *   * ')
-print('  * *  ')
-print('   *   ')
+price = 10 #in programing this number is known as an interger
+rating = 4.9 #in programing this number is known as a float. and the 'rating part is known as a variable.
+name = 'Seiku' #in programing this here 'Seiku' is known as a strig
+is_published = True #in programing these are boolean values 'True/False' 
+print(price)
+
+# these below is me learning about the "the print function".
+#print("Seiku Sillah") 
+#print('o----')
+#print(' ||||')
+#print('*' * 10)
+#print(' ** ** ')
+#print('*  *  *')
+#print(' *   * ')
+#print('  * *  ')
+#print('   *   ')
+
