b2f47eb4a8db3c2a7328fb2e449264c93b6c81e2
sylla-seiku <seikusylla@gmail.com>
2023-09-08 08:44:13 -0400

    Reviwe

--- python.py
+++ python.py
@@ -8,82 +8,82 @@ print(round(x)) # this rounds the number
 print(abs(-2.9)) # "abs" Absolute always returns a positive number.
 
 
-
+'''
 # Learning Operator Precedence
-#x = 10 + 3 * 2
-#print(x)
+x = 10 + 3 * 2
+print(x)
 #Below is the order of operations.
-#parenthesis alwas takes priority
+#parenthesis always takes priority
 #exponentiation 2 ** 3
 #multiplication or division
 #addition or subtraction
-#x = 10 + 3 * 2 ** 2
-#print(x)
-#x = (10 + 3) * 2 ** 2
-#print(x)
+x = 10 + 3 * 2 ** 2
+print(x)
+x = (10 + 3) * 2 ** 2
+print(x)
 
 
 
 #Learning arithmetic Operations
-#print(10 // 3) #if i run it with "/" it gives me a float number. with "//" it gives me an intger
-#print(10 % 3) # this gives me the remender of a division it is called "modulis"
-#print(10 ** 3)# this known as the power
-#x = 10 
-#x = x + 3
-#x += 3 #the augmented assign operator
-#print(x)
+print(10 // 3) #if i run it with "/" it gives me a float number. with "//" it gives me an intger
+print(10 % 3) # this gives me the remender of a division it is called "modulis"
+print(10 ** 3)# this known as the power
+x = 10 
+x = x + 3
+x += 3 #the augmented assign operator
+print(x)
 
 #learning string methods
-#course = 'Python for Beginners'
-#print(len(course)) # "len" counts the number of characters we have in a string
-#print(course.upper())
-#print(course.lower())
-#print(course)
-#print(course.find('P'))
-#print(course.find('o'))
-#print(course.replace('Beginners', 'Absolute Beginners'))
-#print('Python' in course) # the "in" is boolean value 
+course = 'Python for Beginners'
+print(len(course)) # "len" counts the number of characters we have in a string
+print(course.upper())
+print(course.lower())
+print(course)
+print(course.find('P'))
+print(course.find('o'))
+print(course.replace('Beginners', 'Absolute Beginners'))
+print('Python' in course) # the "in" is boolean value 
 
 #learning formated strings
-#first = 'Seiku'
-#last = 'Sillah'
-#message = first + ' [' + last + '] is a coder' #this a concatenation string its harad to visualize.
-#msg = f'{first} [{last}] is a coder' #this is a formated string much easire to visualize.
-#print(msg)
+first = 'Seiku'
+last = 'Sillah'
+message = first + ' [' + last + '] is a coder' #this a concatenation string its harad to visualize.
+msg = f'{first} [{last}] is a coder' #this is a formated string much easire to visualize.
+print(msg)
 
 
 #learning about strings.
-#course = 'Python for "Beginners"'
-#another = course[:]
-#print(another)
+course = 'Python for "Beginners"'
+another = course[:]
+print(another)
 
 #learning type conversion
-#weight_lbs = input('Weight (lbs): ')
-#weight_kg = int(weight_lbs) * 0.45
-#print(weight_kg)
+weight_lbs = input('Weight (lbs): ')
+weight_kg = int(weight_lbs) * 0.45
+print(weight_kg)
 
-#birth_year = input('Birth year: ')
-#print(type(birth_year))
-#age = 2023 - int(birth_year)
-#print(type(age))
-#print(age)
+birth_year = input('Birth year: ')
+print(type(birth_year))
+age = 2023 - int(birth_year)
+print(type(age))
+print(age)
 
 #learning about inputs and expressions
-#name = input('what is your name? ')
-#color = input('what is your favouritr color? ')
-#print(name + ' likes ' + color)
+name = input('what is your name? ')
+color = input('what is your favouritr color? ')
+print(name + ' likes ' + color)
 
 #below i learned about using diffrent variables in a code.
-#full_name = 'Seiku Sillah'
-#age = 30
-#is_new = True
+full_name = 'Seiku Sillah'
+age = 30
+is_new = True
 
 #below here i learned about variables
-#price = 10 #in programing this number is known as an interger
-#rating = 4.9 #in programing this number is known as a float. and the 'rating part is known as a variable.
-#name = 'Seiku' #in programing this here 'Seiku' is known as a strig
-#is_published = True #in programing these are boolean values 'True/False' 
-#print(price)
+price = 10 #in programing this number is known as an interger
+rating = 4.9 #in programing this number is known as a float. and the 'rating part is known as a variable.
+name = 'Seiku' #in programing this here 'Seiku' is known as a strig
+is_published = True #in programing these are boolean values 'True/False' 
+print(price)
 
 # these below is me learning about the "the print function".
 #print("Seiku Sillah") 
@@ -95,4 +95,4 @@ print(abs(-2.9)) # "abs" Absolute always returns a positive number.
 #print(' *   * ')
 #print('  * *  ')
 #print('   *   ')
-
+'''
